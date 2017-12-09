413fd7006b1fafe52c97778ebc765cbb2662d421
Sharat Gajulapalli <sharat.gajulapalli@teamaol.com>
2017-12-09 16:43:46 -0500

    add images

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ console.log('Server started on port 8080');
 function handleRequest(req, res) {
   // What did we request?
   var pathname = req.url;
-  
+
   // If blank let's ask for index.html
   if (pathname == '/') {
     pathname = '/index.html';
   }
-  
+
   // Ok what's our file extension
   var ext = path.extname(pathname);
 
@@ -65,24 +65,39 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection',
   // We are given a websocket object in our function
   function (socket) {
-  
+
     console.log("We have a new client: " + socket.id);
-  
+
     // When this user emits, client side: socket.emit('otherevent',some data);
     socket.on('mouse',
       function(data) {
         // Data comes in as whatever was sent, including objects
         console.log("Received: 'mouse' " + data.x + " " + data.y);
-      
+
         // Send it to all other clients
         socket.broadcast.emit('mouse', data);
-        
+
         // This is a way to send to everyone including sender
         // io.sockets.emit('message', "this goes to everyone");
 
       }
     );
-    
+
+    // When this user emits, client side: socket.emit('otherevent',some data);
+    socket.on('mouse1',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+
+        // Send it to all other clients
+        socket.broadcast.emit('mouse1', data);
+
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
     });
