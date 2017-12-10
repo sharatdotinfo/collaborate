aad89bad081c242b528443a598f616840e904887
Sharat Gajulapalli <sharat.gajulapalli@teamaol.com>
2017-12-10 12:25:06 -0500

    add images

--- server.js
+++ server.js
@@ -83,6 +83,20 @@ io.sockets.on('connection',
       }
     );
 
+    socket.on('mouse1',
+      function(data) {
+        // Data comes in as whatever was sent, including objects
+        console.log("Received: 'mouse' " + data.x + " " + data.y);
+
+        // Send it to all other clients
+        socket.broadcast.emit('mouse', data);
+
+        // This is a way to send to everyone including sender
+        // io.sockets.emit('message', "this goes to everyone");
+
+      }
+    );
+
     socket.on('disconnect', function() {
       console.log("Client has disconnected");
     });
--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 // Keep track of our socket connection
 var socket;
 var img;
+var pg;
 
 function preload() {
     img = loadImage("darth.png");
@@ -8,6 +9,8 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth/2, windowHeight/2);
+  pg = createGraphics(300, 300);
+
   //img = loadImage("darth.png");
   //img.opacity = 0.2;
 
@@ -27,13 +30,32 @@ function setup() {
       ellipse(data.x,data.y,20,20);
     }
   );
+
+  socket.on('mouse1',
+    // When we receive data
+    function(data) {
+      console.log("Got1: " + data.x + " " + data.y);
+      // Draw a blue circle
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x,data.y,20,20);
+    }
+  );
 }
+
+function mousePressed(){
+      pg.fill(255, 0, 0);
+      pg.ellipse(mouseX, mouseY, 20, 20);
+
+      sendmouse1(mouseX,mouseY);
+
+    }
 
 function draw() {
   // Displays the image at its actual size at point (0,0)
-    image(img, 0, 0);
-    // Displays the image at point (0, height/2) at half size
-    //image(img, 0, height/2, img.width/2, img.height/2);
+  image(img, 0, 0, 300, 300);
+    image(pg, 0, 0);
+    image(pg, 0, 300);
   }
 
 function func() {
@@ -73,4 +95,18 @@ function sendmouse(xpos, ypos) {
 
   // Send that object to the socket
   socket.emit('mouse',data);
+}
+
+function sendmouse1(xpos, ypos) {
+  // We are sending!
+  console.log("sendmouse1: " + xpos + " " + ypos);
+
+  // Make a little object with  and y
+  var data = {
+    x: xpos,
+    y: ypos
+  };
+
+  // Send that object to the socket
+  socket.emit('mouse1',data);
 }
