987fdaed62d0aba12583fdc7271663ce81872c79
Sharat Gajulapalli <sharat.gajulapalli@teamaol.com>
2017-12-10 12:48:53 -0500

    add images

--- sketch.js
+++ sketch.js
@@ -40,10 +40,8 @@ function setup() {
       //noStroke();
       //ellipse(data.x,data.y,20,20);
 
-      pg = createGraphics(300, 300);
-
       pg.fill(255, 0, 0);
-      pg.ellipse(mouseX, mouseY, 20, 20);
+      pg.ellipse(data.x, data.y, 20, 20);
     }
   );
 }
